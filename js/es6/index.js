472c2e00ec727fd2772e979f8a06610856f0e457
menim <mel198629@gmail.com>
2019-01-04 15:36:01 +0200

    add smooth fade in for wht-sketch and video components when they are in view

--- js/es6/index.js
+++ js/es6/index.js
@@ -1,22 +1,3 @@
-// const mySwiper = new Swiper('.swiper-container', {
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '',
-//     prevEl: '',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '',
-//   },
-// });
-
 document.querySelectorAll('.swiper-container').forEach(item => {
   new Swiper(item, {
     // If we need pagination
@@ -36,3 +17,36 @@ document.querySelectorAll('.swiper-container').forEach(item => {
     },
   });
 });
+
+const videoEl = document.querySelector('.video');
+const sketchEl = document.querySelector('.wht-sketch');
+const descriptCont = document.querySelector('.description');
+
+let descriptContYPos = Math.round(descriptCont.getBoundingClientRect().top);
+
+function debounce(func, wait = 10, immediate = true) {
+  let timeout;
+  return function () {
+    let context = this, args = arguments;
+    let later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+function checkPosition() {
+  let windowY = window.scrollY;
+  if(windowY >= descriptContYPos) {
+    videoEl.classList.add('is-visible');
+    sketchEl.classList.add('is-visible');
+    videoEl.classList.remove('is-hidden');
+    sketchEl.classList.remove('is-hidden');
+  }
+}
+
+window.addEventListener('scroll', debounce(checkPosition));
--- js/index.js
+++ js/index.js
@@ -1,24 +1,5 @@
 'use strict';
 
-// const mySwiper = new Swiper('.swiper-container', {
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '',
-//     prevEl: '',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '',
-//   },
-// });
-
 document.querySelectorAll('.swiper-container').forEach(function (item) {
   new Swiper(item, {
     // If we need pagination
@@ -38,3 +19,40 @@ document.querySelectorAll('.swiper-container').forEach(function (item) {
     }
   });
 });
+
+var videoEl = document.querySelector('.video');
+var sketchEl = document.querySelector('.wht-sketch');
+var descriptCont = document.querySelector('.description');
+
+var descriptContYPos = Math.round(descriptCont.getBoundingClientRect().top);
+
+function debounce(func) {
+  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+  var timeout = void 0;
+  return function () {
+    var context = this,
+        args = arguments;
+    var later = function later() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+function checkPosition() {
+  var windowY = window.scrollY;
+  if (windowY >= descriptContYPos) {
+    videoEl.classList.add('is-visible');
+    sketchEl.classList.add('is-visible');
+    videoEl.classList.remove('is-hidden');
+    sketchEl.classList.remove('is-hidden');
+  }
+}
+
+window.addEventListener('scroll', debounce(checkPosition));
