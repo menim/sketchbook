f58ac63c2e4d5127de661e7e112f0e1912afb34a
tol <tol@Mac-mini-tol.local>
2019-01-16 09:48:05 +0200

    add feature that yotube video is loading only when click play button and refactoring description revaling feature

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 document.querySelectorAll('.swiper-container').forEach(function (item) {
+  // eslint-disable-next-line no-new
   new Swiper(item, {
     // If we need pagination
     pagination: {
@@ -23,36 +24,66 @@ document.querySelectorAll('.swiper-container').forEach(function (item) {
 var videoEl = document.querySelector('.video');
 var sketchEl = document.querySelector('.wht-sketch');
 var descriptCont = document.querySelector('.description');
-
-var descriptContYPos = Math.round(descriptCont.getBoundingClientRect().top);
-
-function debounce(func) {
-  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
-  var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
-
-  var timeout = void 0;
-  return function () {
-    var context = this,
-        args = arguments;
-    var later = function later() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
+var viewHeight = window.innerHeight || document.documentElement.clientHeight;
 
 function checkPosition() {
-  var windowY = window.scrollY;
-  if (windowY >= descriptContYPos) {
+  var descriptContYPos = descriptCont.getBoundingClientRect().top;
+  if (viewHeight >= descriptContYPos) {
     videoEl.classList.add('is-visible');
     sketchEl.classList.add('is-visible');
     videoEl.classList.remove('is-hidden');
     sketchEl.classList.remove('is-hidden');
+    window.removeEventListener('scroll', checkPosition);
   }
 }
+window.addEventListener('scroll', checkPosition);
 
-window.addEventListener('scroll', debounce(checkPosition));
+function findVideo() {
+  var video = document.querySelector('.video');
+  setupVideo(video);
+}
+
+function setupVideo(video) {
+  var link = video.querySelector('.video__link');
+  var media = video.querySelector('.video__media');
+  var button = video.querySelector('.video__button');
+  var id = parseMediaURL(media);
+
+  video.addEventListener('click', function () {
+    var iframe = createIframe(id);
+
+    link.remove();
+    button.remove();
+    video.appendChild(iframe);
+  });
+
+  link.removeAttribute('href');
+  video.classList.add('video--enabled');
+}
+
+function parseMediaURL(media) {
+  var regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/sddefault\.jpg/i;
+  var url = media.src;
+  var match = url.match(regexp);
+
+  return match[1];
+}
+
+function createIframe(id) {
+  var iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
+  iframe.setAttribute('src', generateURL(id));
+  iframe.classList.add('video__media');
+
+  return iframe;
+}
+
+function generateURL(id) {
+  var query = '?rel=0&modestbranding=1&autohide=1&showinfo=0&autoplay=1';
+
+  return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideo();
