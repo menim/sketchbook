5ec4caafd0c5902cc27859e29fa9c90a910f5408
tol <tol@Mac-mini-tol.local>
2019-01-28 13:13:53 +0200

    add custom tiny-slider navigation to product page

--- js/es6/product.js
+++ js/es6/product.js
@@ -1,9 +1,12 @@
 let slider = tns({
   container: '.slider-container',
+  controlsContainer: '.product__view',
+  navContainer: '.nav-customize',
   gutter: 25,
   slideBy: 1,
   items: 1,
   controls: false,
   navPosition: 'bottom',
+  navAsThumbnails: true,
   mouseDrag: true
 });
